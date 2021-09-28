450e6893f41db6ee410fdc39f2fa3285859812b0
Shatish Desai <sdondjango@gmail.com>
2021-09-28 23:11:17 +0530

    test

--- src/component/Button.js
+++ src/component/Button.js
@@ -3,6 +3,11 @@ import React from "react";
 const Button = ({ children, loading, ...props }) => {
   return (
     <button disabled={loading} {...props}>
+
+
+
+
+    
       {loading ? "Loading..." : children}
     </button>
   );
